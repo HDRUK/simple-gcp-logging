ae07215cc9dca0708738429f241d629eec9055f6
Dan Nita <dan.nita@hdruk.ac.uk>
2021-11-15 11:45:43 +0000

    update package json

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const LoggerGCP = require('./classes/logger');
 
-module.exports = LoggerGCP;
+module.exports.createLoggerGCP = (config) => {
+    return new LoggerGCP(config);
+}
+
+// module.exports = LoggerGCP;
